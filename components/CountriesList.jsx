b35709b67f3b6020ddf62dd7249076b99eaf204a
smriti <rajsimmi369@gmail.com>
2025-02-06 23:16:22 +0530

    final code done

--- components/CountriesList.jsx
+++ components/CountriesList.jsx
@@ -3,9 +3,17 @@ import CountriesData from "../CountriesData";
 import CountryCard from "./CountryCard";
 
 export default function CountriesList() {
-  const array = CountriesData.map((country) => {
-    console.log(country)
-    return <CountryCard name={country.name.common} flag={country.flags.svg} />;
+  const array = CountriesData.map((country, i) => {
+    return (
+      <CountryCard
+        key={i}
+        name={country.name.common}
+        flag={country.flags.svg}
+        population={country.population.toLocaleString("en-IN")}
+        region={country.region}
+        capital={country.capital?.[0]}
+      />
+    );
   });
 
   return <div className="countriesContainer">{array}</div>;
--- components/CountryCard.jsx
+++ components/CountryCard.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 
-export default function CountryCard({name,flag})
- {
-  
+export default function CountryCard({
+  name,
+  flag,
+  population,
+  region,
+  capital,
+}) {
   return (
-    <a className="country-card" href="/country.html?name=Grenada">
+    <a
+      className="country-card"
+      href={`/country.html?name=${encodeURIComponent(name)}`}
+    >
       <img src={flag} alt={name + `flag`} />
       <div className="card-text">
         <h3 className="card-title">{name}</h3>
         <p>
-          <b>Population: </b>1,12,519
+          <b>Population: </b>
+          {population}
         </p>
         <p>
-          <b>Region: </b>Americas
+          <b>Region: </b>
+          {region}
         </p>
         <p>
-          <b>Capital: </b>St. George's
+          <b>Capital: </b>
+          {capital}
         </p>
       </div>
     </a>
